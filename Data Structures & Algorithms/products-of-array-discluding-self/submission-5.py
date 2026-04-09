05334845f51ffeb9b9d6e2dd7f0a4a50b1639de0
henryxie415 <156744669+henryxie415@users.noreply.github.com>
2026-04-09 10:57:01 -0700

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        #find the products of the left side except for i 
+        #find the products of the right side except for i 
+        #multiply them together and append to the final array
+        n = len(nums)
+        arr = [1] * n
+        left = 1
+        for i in range(n):
+            arr[i] *= left 
+            left *= nums[i]
+
+        right = 1
+        for i in range(n-1,-1,-1):
+            arr[i] *= right
+            right *= nums[i]
+        return arr
